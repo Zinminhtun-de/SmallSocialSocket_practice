0da12b6fe20dca7b97248ab86bbe7a927afd0a86
zinminhtun <zinminhtunok591997@gmail.com>
2022-04-19 23:54:58 +0630

    Build socket practice

---
+++ client/src/data.js
@@ -0,0 +1,47 @@
+export const posts = [
+  {
+    id: 1,
+    username: "maryar",
+    fullname: "Maryar",
+    userImg:
+      "https://t3.ftcdn.net/jpg/02/79/27/28/240_F_279272810_meTFBlpWMMpfncurWmMji4OeefCKneJQ.jpg",
+    postImg:
+      "https://t3.ftcdn.net/jpg/00/71/07/46/240_F_71074666_kUiqjWUKwm6pEgG3qIcZsIAtWWMtcoBD.jpg",
+  },
+  {
+    id: 2,
+    username: "gwasar",
+    fullname: "Gwasar",
+    userImg:
+      "https://t4.ftcdn.net/jpg/01/12/60/49/240_F_112604913_W193pzN8L3IeSggUezTbx5Vg70lvT6tq.jpg",
+    postImg:
+      "https://t4.ftcdn.net/jpg/02/30/08/79/240_F_230087928_D2SvwrfXALafi5rWv9J1fm4TykNRF57I.jpg",
+  },
+  {
+    id: 3,
+    username: "barmyar",
+    fullname: "Barmyar",
+    userImg:
+      "https://t4.ftcdn.net/jpg/01/12/60/49/240_F_112604913_W193pzN8L3IeSggUezTbx5Vg70lvT6tq.jpg",
+    postImg:
+      "https://t4.ftcdn.net/jpg/02/30/08/79/240_F_230087928_D2SvwrfXALafi5rWv9J1fm4TykNRF57I.jpg",
+  },
+  {
+    id: 4,
+    username: "hlv lhv",
+    fullname: "Hlv Lhv",
+    userImg:
+      "https://t4.ftcdn.net/jpg/01/12/60/49/240_F_112604913_W193pzN8L3IeSggUezTbx5Vg70lvT6tq.jpg",
+    postImg:
+      "https://t4.ftcdn.net/jpg/02/30/08/79/240_F_230087928_D2SvwrfXALafi5rWv9J1fm4TykNRF57I.jpg",
+  },
+  {
+    id: 5,
+    username: "gerq",
+    fullname: "Gerq",
+    userImg:
+      "https://t4.ftcdn.net/jpg/01/12/60/49/240_F_112604913_W193pzN8L3IeSggUezTbx5Vg70lvT6tq.jpg",
+    postImg:
+      "https://t4.ftcdn.net/jpg/02/30/08/79/240_F_230087928_D2SvwrfXALafi5rWv9J1fm4TykNRF57I.jpg",
+  },
+];
